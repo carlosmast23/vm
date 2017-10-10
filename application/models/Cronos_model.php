1d869e142533a9643d1620c5eb41e44f9d516e71
jc100pl2017 <juancarlos100pl@hotmail.com>
2017-10-10 11:31:24 -0500

    Servidor de correos

--- application/models/Cronos_model.php
+++ application/models/Cronos_model.php
@@ -195,6 +195,7 @@ public function insertar_sms($bus_id,$tel_destinatario,$sms,$deque){
 public function procesar_email_prov(){
   $this->load->library('My_PHPMailer');
 
+
   $mail = new PHPMailer();
         $mail->IsSMTP(); // we are going to use SMTP
         $mail->CharSet = 'UTF-8';
@@ -213,38 +214,121 @@ public function procesar_email_prov(){
         $mail->Username   = "virtualmallecu@gmail.com";  // user email address
         $mail->Password   = "code17bwbtj";            // password in GMail
         $mail->SetFrom('virtualmallecu@gmail.com', 'Servidor Correos VM');  //Who is sen
+        $mail->Subject    = "Prueba de email";
 
         $sql="SELECT * FROM `envio_email` WHERE `estado`='p' AND (`deque`='p' OR `deque` LIKE 'cp') ";
         $query=$this->db->query($sql);
         if($query->num_rows()>0){
           foreach ($query->result() as $fila) {
-           $usuario=datoDeTablaCampo("proveedores","prv_id","prv_usuario",$fila->usu_id);
-           
-           $mail->Subject    = $fila->asunto;
-           $mail->Body      = $fila->mensaje;
-           $mail->AltBody    = "VirtuallMall";
-           $mail->AddAddress($fila->email_destinatario, $usuario);
 
-           if(!$mail->Send()){ 
-             log_message("error",$mail->ErrorInfo);
-             echo "Error: " . $mail->ErrorInfo; 
-           }else 
-           echo "Message sent correctly!";  
-           $mail->clearAddresses();
 
-           $this->db->where("id",$fila->id);
-           $this->db->update("envio_email",array("estado"=>'e'));    
+   $mail->AddAddress($destino, "Juan Pinargo");
 
-         }
-
-       }
-
+        $mail->AddAttachment(base_url()."img/logo.png");      // some attached files
+       // $mail->AddAttachment("images/phpmailer_mini.gif"); // as many as you want
+        if(!$mail->Send()) {
+          echo "Error: " . $mail->ErrorInfo; //  $data["message"] = "Error: " . $mail->ErrorInfo;
+      } else {
+         echo "Message sent correctly!";  // $data["message"] = "Message sent correctly!";
      }
+            
+            $this->db->where("id",$fila->id);
+            $this->db->update("envio_email",array("estado"=>'e'));    
+          }
+
+        }
+
+      }
+
+
+/*
+  public function procesar_sms_clipendientes(){
+    $sql="SELECT * FROM `envio_sms` WHERE `deque`='c' AND `bus_id` NOT IN(SELECT `bus_id` FROM propuestas WHERE `pro_estado`='p') ";
+
+    $query=$this->db->query($sql);
+    if($query->num_rows()>0){
+      foreach ($query->result() as $fila) {
+        $bus_fecha=datoDeTablaCampo("busquedas","bus_id","bus_fecha",$fila->bus_id);
+        $bus_tiempo=datoDeTablaCampo("busquedas","bus_id","bus_tiempo",$fila->bus_id);
+        $bus_celular=datoDeTablaCampo("busquedas","bus_id","bus_celular",$fila->bus_id);
+
+        if(diamas($bus_fecha,$bus_tiempo) > hoy('c')){
+
+
+          $error = $cliente->getError();
+          if ($error) 
+            log_message('error', 'ERROR WEBSERVICE.'.$error);
+          else{
+            $sms="Su busqueda no obtuvo resultados, genere una nueva solictud (amplie su tiempo de respuesta)";
+            $result = $cliente->call("enviarSMS",array($fila->tel_destinatario,$sms));
+            //$this->db->where("id",$fila->id);
+            //$this->db->update("envio_sms",array("estado"=>'r'));
+
+            $d1=array(
+              "bus_id"=>$fila->bus_id,
+              'ser_id' => 1, 
+              "usu_id"=>0,
+              "tel_destinatario"=>$fila->tel_destinatario,
+              "mensaje"=>$sms,
+              "fecha"=>hoy('c'),
+              "deque"=>"cp",
+              "estado"=>"e",
+              );
+            $this->db->insert("envio_sms",$d1);
+
+          }
+        }
+
+
+      }
+    }
+
+  }
+
+  public function procesar_sms_cliterminados(){
+    $sql="SELECT * FROM `envio_sms` WHERE `deque`='c' AND `bus_id` IN(SELECT `bus_id` FROM propuestas WHERE `pro_estado`='p') ";
+
+    $query=$this->db->query($sql);
+    if($query->num_rows()>0){
+      foreach ($query->result() as $fila) {
+        $bus_fecha=datoDeTablaCampo("busquedas","bus_id","bus_fecha",$fila->bus_id);
+        $bus_tiempo=datoDeTablaCampo("busquedas","bus_id","bus_tiempo",$fila->bus_id);
+        $bus_celular=datoDeTablaCampo("busquedas","bus_id","bus_celular",$fila->bus_id);
+
+        if(diamas($bus_fecha,$bus_tiempo) == hoy('c')){
+
+          $error = $cliente->getError();
+          if ($error) 
+            log_message('error', 'ERROR WEBSERVICE.'.$error);
+          else{
+            $sms="Se ha finalizado la busqueda de resultados, esperamos que alguna propuesta haya sido de tu agrado";
+            $result = $cliente->call("enviarSMS",array($fila->tel_destinatario,$sms));
+
+            $d1=array(
+              "bus_id"=>$fila->bus_id,
+              'ser_id' => 1, 
+              "usu_id"=>0,
+              "tel_destinatario"=>$fila->tel_destinatario,
+              "mensaje"=>$sms,
+              "fecha"=>hoy('c'),
+              "deque"=>"ct",
+              "estado"=>"e",
+              );
+            $this->db->insert("envio_sms",$d1);
+
+
+          }
+        }
+
+
+      }
+    }
+
+  }
+  */
 
 
 
 
 
-
-
-   }
+}
