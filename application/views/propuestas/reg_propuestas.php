c64f5709bfd3537bca46fcc15be4c4aa9b1dc1f4
jc100pl2017 <juancarlos100pl@hotmail.com>
2017-10-24 16:29:38 -0500

    Correccion registro de propuestas y envio de SMS

--- application/views/propuestas/reg_propuestas.php
+++ application/views/propuestas/reg_propuestas.php
@@ -69,13 +69,13 @@
 						<div class="col-md-6">
 							<div class="form-group">
 								<label>Tipo:</label><br>
-								<input type="radio" name="pro_tipo" value="n" checked /> Nuevo
-								<input type="radio" name="pro_tipo" value="u" /> Usado
+								<input type="radio" id="pro_tipo_n" value="n" checked /> Nuevo
+								<input type="radio" id="pro_tipo_u" value="u" /> Usado
 							</div>
 							<div class="form-group">
 								<label>Entrega a domicilio:</label><br>
-								<input type="radio" name="pro_entrega" value="n" checked /> No
-								<input type="radio" name="pro_entrega" value="s" /> Si
+								<input type="radio" id="pro_entrega_n" value="n" checked /> No
+								<input type="radio" id="pro_entrega_s" value="s" /> Si
 							</div>
 							<div class="form-group">
 								<label>Imagen adjunta:</label><br>
@@ -83,8 +83,10 @@
 							</div>
 						</div>
 						<div class="col-md-6">
-							<input type="hidden" name="bus_id" value="<?=$bus_id?>">
-							<input type="hidden" name="prv_id" value="<?=$prv_id?>">
+							<input type="hidden" name="pro_tipo" id="pro_tipo">
+							<input type="hidden" name="pro_entrega" id="pro_entrega">
+							<input type="hidden" name="bus_id" id="bus_id" value="<?=$bus_id?>">
+							<input type="hidden" name="prv_id" id="prv_id" value="<?=$prv_id?>">
 							<button type="submit" class="btn btn-success btn-block" id="almacenar">ENVIAR</button>
 						</div>
 					</form>
@@ -120,14 +122,30 @@
 		}
 	});
 
-	/*$( "#form_almacenar" ).submit(
-		function( event ) 
-		{
-			var form = $( "#form_almacenar" );
-			var isValid = form.valid();
-			alert(isValid);
-		}
-	);*/
+	$("#pro_tipo_n").click(function () {
+		$("#pro_tipo").val("n");
+	});
+	$("#pro_tipo_u").click(function () {
+		$("#pro_tipo").val("u");
+	});
+	$("#pro_entrega_n").click(function () {
+		$("#pro_entrega").val("n");
+	});
+	$("#pro_entrega_s").click(function () {
+		$("#pro_entrega").val("s");
+	});
+	if ($('#pro_tipo_n').prop('checked')) {
+		$("#pro_tipo").val("n");
+	}
+	if ($('#pro_tipo_u').prop('checked')) {
+		$("#pro_tipo").val("u");
+	}
+	if ($('#pro_entrega_n').prop('checked')) {
+		$("#pro_entrega").val("n");
+	}
+	if ($('#pro_entrega_s').prop('checked')) {
+		$("#pro_entrega").val("s");
+	}
 
 	//Implementar el metodo submit del formulario para agregar manualmente los datos   
 	$( "#form_almacenar" ).submit(
@@ -137,22 +155,24 @@
 			if(!form.valid())
 				return false; // Evita que si el formulario no esta validado no continuar con el envio de datos
 
-		    var request = new XMLHttpRequest();
+			var request = new XMLHttpRequest();
 
 		    // Metodo que direcciona a la pagina correcta si el proceso es normal
 		    request.addEventListener("load", function(event) 
 		    {
-		    	alert('carga terminada');
-		        window.location.href = "<?=base_url()?>propuestas/successp";
+		    	console.log('carga terminada');
+		    	window.location.href = "<?=base_url()?>propuestas/successp";
 		    });
 
 		 	// Metodo que direcciona a la pagina de error si el proceso es incorrecto
-		    request.addEventListener("error", function(event) 
-		    {
-		    	alert('carga con error');
+		 	request.addEventListener("error", function(event) 
+		 	{
+		 		console.log('carga con error');
 		    	//Falta implementar  
 		    });
-	    	
+
+
+
 	    	//Objeto del formulario creado desde 0 seteando las propiedades del formulario
 	    	var formData = new FormData();
 	    	formData.append("prv_id",$("#prv_id").val());
@@ -168,13 +188,13 @@
 	    	formData.append("userfile",blobFile);
 
 	    	//Configurando metodo de envio y direccion
-	 		request.open("POST", "<?=base_url()?>propuestas/registrar_propuesta");
-			request.send(formData);
+	    	request.open("POST", "<?=base_url()?>propuestas/registrar_propuesta");
+	    	request.send(formData);
 
 			//Cancelar el evento por defecto de la funcion
 			event.preventDefault();
 		}
-	);
+		);
 
 	//Implementado el metodo cuando se realiza algun cambio en el file
 	$( "#userfileTemp" ).change(
@@ -184,8 +204,8 @@
 			fileTemp=this.files[0]
 
 			//Libreria que permie modificar las dimensiones de la imagen
-  			ImageTools.resize(this.files[0], 
-		    {
+			ImageTools.resize(this.files[0], 
+			{
 		        width: 800, // maximum width
 		        height: 800 // maximum height
 		    }, 
@@ -194,10 +214,10 @@
 		    	//Blob.- es el archivo sin formato reducido el tamaño
 		    	//didItResize .- Variable booleana que devuelve true si la imagen se modifico o false si no sufrio transformacion	    	
 		    	//Convertir el archivo blob a File para agregar al formulario posteriormente
-		        blobFile = new File([blob], fileTemp.name, {type: fileTemp.type, lastModified: Date.now()});
+		    	blobFile = new File([blob], fileTemp.name, {type: fileTemp.type, lastModified: Date.now()});
 
 		    });
 		}
-	);
+		);
 
-</script>
+	</script>
