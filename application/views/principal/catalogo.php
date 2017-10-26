4a966e33c6b681ad4cf757a502a8f0e85f4d6976
jc100pl2017 <juancarlos100pl@hotmail.com>
2017-10-26 17:35:53 -0500

    Actualizacion de catalogo movil

--- application/views/principal/catalogo.php
+++ application/views/principal/catalogo.php
@@ -3,48 +3,49 @@
         background:#ccc;
     }
     #magazine{
+        display: block;
+        margin: auto;
         width:1152px;
         height:752px;
     }
     #magazine .turn-page{
         background-color:#ccc;
-        background-size:90% 90%;
+        background-size:100% 100%;
     }
     #magazine div{
-        background-size: 100%;
+        background-size: 100% 100%;
     }
 </style>
 <script type="text/javascript" src="<?=base_url()?>js/turn.js"></script>
 
 <!-- Main container -->
-<div id="magazine">
 
-    <div style="background-image:url(<?=base_url()?>/img/pages/pagina0.jpg)"></div>
-    <div style="background-image:url(<?=base_url()?>/img/pages/pagina.jpg)"></div>
-    <div style="background-image:url(<?=base_url()?>/img/pages/pagina1.jpg)"></div>
-    <div style="background-image:url(<?=base_url()?>/img/pages/pagina2.jpg)"></div>
-    <div style="background-image:url(<?=base_url()?>/img/pages/pagina3.jpg)"></div>
-    <div style="background-image:url(<?=base_url()?>/img/pages/pagina4.jpg)"></div>
-    <div style="background-image:url(<?=base_url()?>/img/pages/pagina5.jpg)"></div>
-    <div style="background-image:url(<?=base_url()?>/img/pages/pagina6.jpg)"></div>
-    <div style="background-image:url(<?=base_url()?>/img/pages/pagina7.jpg)"></div>
-    <div style="background-image:url(<?=base_url()?>/img/pages/pagina.jpg)"></div>
-    <div style="background-image:url(<?=base_url()?>/img/pages/pagina8.jpg)"></div>
-
-
-</div>
+    <div class="container" >
+    <div id="magazine">
+        <div style="background-image:url(<?=base_url()?>/img/pages/pagina0.jpg)"></div>
+        <div style="background-image:url(<?=base_url()?>/img/pages/pagina.jpg)"></div>
+        <div style="background-image:url(<?=base_url()?>/img/pages/pagina1.jpg)"></div>
+        <div style="background-image:url(<?=base_url()?>/img/pages/pagina2.jpg)"></div>
+        <div style="background-image:url(<?=base_url()?>/img/pages/pagina3.jpg)"></div>
+        <div style="background-image:url(<?=base_url()?>/img/pages/pagina4.jpg)"></div>
+        <div style="background-image:url(<?=base_url()?>/img/pages/pagina5.jpg)"></div>
+        <div style="background-image:url(<?=base_url()?>/img/pages/pagina6.jpg)"></div>
+        <div style="background-image:url(<?=base_url()?>/img/pages/pagina7.jpg)"></div>
+        <div style="background-image:url(<?=base_url()?>/img/pages/pagina.jpg)"></div>
+        <div style="background-image:url(<?=base_url()?>/img/pages/pagina8.jpg)"></div>
+    </div>
+    </div>
 
 
 <script type="text/javascript">
 
     $(window).ready(function() {
         $('#magazine').turn({
-            display: 'double',
             autoCenter: true,
             acceleration: true,
             gradients: !$.isTouch,
             elevation:1
-       
+
         });
     });
     
