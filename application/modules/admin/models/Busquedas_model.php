ed3ac7e0e8b03a07275a5f443755248b0cffa715
jc100pl2017 <juancarlos100pl@hotmail.com>
2017-10-31 11:52:50 -0500

    Actualizacion cronos

--- application/modules/admin/models/Busquedas_model.php
+++ application/modules/admin/models/Busquedas_model.php
@@ -16,7 +16,7 @@ class Busquedas_model extends CI_Model {
 		$html="";
 		$w_buscar = $this->input->post('buscar');
 
-		$sql = "SELECT * FROM `busquedas` WHERE `bus_estado`='p' ORDER BY `bus_fechafin` DESC";
+		$sql = "SELECT * FROM `busquedas` WHERE `bus_estado`='p' ORDER BY `bus_fechafin` ASC";
 
 		$query = $this->db->query($sql);
 
