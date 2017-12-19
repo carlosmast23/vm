9cae2042848a8e9d0c14d3e716e0b0b4e4f3009c
jc100pl2017 <juancarlos100pl@hotmail.com>
2017-12-19 15:28:36 -0500

    Actualizacion pagina conocenos

--- application/views/principal/conocenos.php
+++ application/views/principal/conocenos.php
@@ -9,7 +9,7 @@
         <div class="embed-responsive embed-responsive-16by9">
           <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/jk4BntyPKD4?autoplay=1"> </iframe>
         </div>
-        
+
         <img src="<?=base_url()?>img/c1.png" class="img-thumbnail" alt="" />
         <img src="<?=base_url()?>img/c2.png" class="img-thumbnail" alt="" />
 
@@ -24,39 +24,51 @@
       <p>
         Virtual Mall nace como un proyecto social que pretende promover el crecimiento socio económico en el Ecuador. El objetivo principal es impulsar el crecimiento de negocios locales, emprendimientos, pequeñas y medianas empresas en fin de forjar una red de clientes, proveedores a nivel local a través del uso de las tecnologías de la información y comunicación.
       </p>
-
-
-      <h4>Servicio de publicación</h4>
       <p>
-        Nuestro servicio de publicación no tiene ningún costo, Virtuall Mall tiene como objetivo anunciar a los negocios locales en fin de forjar una red de proveedores locales. Actualmente este servicio esta disponible en Ecuador, Ciudad Quito y Sangolqui </p>
+      El equipo de trabajo esta conformado por un grupo de profesionales especializados en diversas áreas: sistemas, diseño, publicidad, marketing, contabilidad entre otros. Mediante un trabajo conjunto y el aporte de ideas en beneficio de la sociedad se desarrolla el proyecto Virtual Mall un producto con impacto social que pretender ayudar a promover los negocios de la localidad.
+      </p>
+      <h4>Funcionamiento</h4>
+      
+        <ol>
+        <li>Ingresa a www.vm.codesoft-ec.com busca lo que necesites.</li>
+         <li>Nuestros #proveedores generan y envian sus propuestas via #sms.</li>
+         <li>Escoge la mejor #oferta. </li>
+         <li>El proveedor se pondrá en contacto contigo para concretar la venta.</li>
+       </ol>
 
-        <p>
-         Virtual Mall te invita a aprovechar nuestras promociones, recibe ofertas, descuentos y participar en sorteos de paquetes de publicidad en nuestro servicios unicamente registrandote.Adicionalmente el registro te permite formar parte de nuestra cadena de clientes, proveedores y negocios locales. <a href="<?=base_url()?>general/registro">Registro aqui</a>
-       </p>
+       * Si eres #proveedor esta es la oportunidad de incrementar tus #ventas
 
-       <ul class="social-ul">
-        <li class="box-social"><a href="#0"><i class="ion-social-facebook"></i></a></li>
-        <li class="box-social"><a href="#0"><i class="ion-social-instagram-outline"></i></a></li>
-        <li class="box-social"><a href="#0"><i class="ion-social-twitter"></i></a></li>
-        <li class="box-social"><a href="#0"><i class="ion-social-dribbble"></i></a></li>
-      </ul>
+     <h4>Servicio de publicación</h4>
+     <p>
+      Nuestro servicio de publicación no tiene ningún costo, Virtuall Mall tiene como objetivo anunciar a los negocios locales en fin de forjar una red de proveedores locales. Actualmente este servicio esta disponible en Ecuador, Ciudad Quito y Sangolqui </p>
 
-      <div class="row-fluid">
-        <div class="span8">
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.7513343184114!2d-78.4492166857051!3d-0.3320119997577525!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x91d5bce188eb2f1f%3A0x8bb3f45a038d241b!2sCodesoft!5e0!3m2!1ses!2sec!4v1509128473197" width="100%" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
-        </div>
+      <p>
+       Virtual Mall te invita a aprovechar nuestras promociones, recibe ofertas, descuentos y participar en sorteos de paquetes de publicidad en nuestro servicios unicamente registrandote.Adicionalmente el registro te permite formar parte de nuestra cadena de clientes, proveedores y negocios locales. <a href="<?=base_url()?>general/registro">Registro aqui</a>
+     </p>
 
-        <div class="span4">
-          <h2>Encuentranos</h2>
-          <address>
-            <strong>General Enriquez y Espejo</strong><br>
-            Local 4<br>
-            <p><b>Email:</b> info@codesoft-ec.com  -  virtualmallecu@gmail.com</p>
+     <ul class="social-ul">
+      <li class="box-social"><a href="#0"><i class="ion-social-facebook"></i></a></li>
+      <li class="box-social"><a href="#0"><i class="ion-social-instagram-outline"></i></a></li>
+      <li class="box-social"><a href="#0"><i class="ion-social-twitter"></i></a></li>
+      <li class="box-social"><a href="#0"><i class="ion-social-dribbble"></i></a></li>
+    </ul>
 
-            <abbr title="Phone">Teléfono:</abbr> 2 339 487
-          </address>
-        </div>
+    <div class="row-fluid">
+      <div class="span8">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.7513343184114!2d-78.4492166857051!3d-0.3320119997577525!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x91d5bce188eb2f1f%3A0x8bb3f45a038d241b!2sCodesoft!5e0!3m2!1ses!2sec!4v1509128473197" width="100%" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+      </div>
+
+      <div class="span4">
+        <h2>Encuentranos</h2>
+        <address>
+          <strong>General Enriquez y Espejo</strong><br>
+          Local 4<br>
+          <p><b>Email:</b> info@codesoft-ec.com  -  virtualmallecu@gmail.com</p>
+
+          <abbr title="Phone">Teléfono:</abbr> 2 339 487
+        </address>
       </div>
     </div>
   </div>
-  <!-- end Main container -->
+</div>
+<!-- end Main container -->
