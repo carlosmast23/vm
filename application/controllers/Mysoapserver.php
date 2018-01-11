e9bdb87372e4f717ebe6ab5dafab5e73dd624e99
jc100pl2017 <juancarlos100pl@hotmail.com>
2018-01-11 13:48:55 -0500

    actualizacion webservice

--- application/controllers/Mysoapserver.php
+++ application/controllers/Mysoapserver.php
@@ -9,14 +9,14 @@ class Mysoapserver extends MY_Controller {
         $ns=base_url()."mysoapserver";
         $this->load->library("Nusoap_lib"); //load the library here
         $this->nusoap_server = new soap_server();
-        $this->nusoap_server->configureWSDL("SOAPServer", $ns,false,"document");
+        $this->nusoap_server->configureWSDL("SOAPServer", $ns);//false,"document"
         $this->nusoap_server->wsdl->schemaTargetNamespace = $ns;
 
         //registrando funciones
 
         $input_array2 = array ('celular' => "xsd:string", 'buscar' => "xsd:string",'time' => "xsd:string");
         $return_array2= array ("return" => "xsd:string");
-        $this->nusoap_server->register('buscarvm', $input_array2, $return_array2, "urn:SOAPServerWSDL", "urn:".$ns."/buscarvm", "document", "literal", "Buscar en Virtual Mall");
+        $this->nusoap_server->register('buscarvm', $input_array2, $return_array2, "urn:SOAPServerWSDL", "urn:".$ns."/buscarvm", "rpc", "encoded", "Buscar en Virtual Mall");
 
     }
 
