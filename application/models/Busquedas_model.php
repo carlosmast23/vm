e2e6e1a6884028d5554fd572d82d114b1a29f676
jc100pl2017 <juancarlos100pl@hotmail.com>
2018-01-15 10:41:39 -0500

    correcion funcion limpiar texto

--- application/models/Busquedas_model.php
+++ application/models/Busquedas_model.php
@@ -24,7 +24,7 @@ class Busquedas_model extends CI_Model {
    $data=array(
     "act_id"=>0,
     "bus_celular"=>$ncel,
-    "bus_texto"=>$buscar,
+    "bus_texto"=>limpiar_texto($buscar),
     "bus_fecha"=>$hoy,
     "bus_fechafin"=>diamas($hoy,$tiempo),
     "bus_tiempo"=>$tiempo,
@@ -56,7 +56,7 @@ public function buscar2_mdl($txt_celular="",$tx_buscar="",$time="") {
  $data=array(
   "act_id"=>0,
   "bus_celular"=>$ncel,
-  "bus_texto"=>$buscar,
+  "bus_texto"=>limpiar_texto($buscar),
   "bus_fecha"=>$hoy,
   "bus_fechafin"=>diamas($hoy,$tiempo),
   "bus_tiempo"=>$tiempo,
